c53243339698a355d76b8b9c837a67a9cee84800
Kuru-teo <mmmorohoshi@gmail.com>
2022-06-18 20:42:43 +0900

    resource追加

---
+++ ThemeColorPyside2/MyPyside2Lib/imageResource.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# Resource object code
+#
+# Created: �� 8 5 11:52:38 2021
+#      by: The Resource Compiler for PySide2 (Qt v4.8.7)
+#
+# WARNING! All changes made in this file will be lost!
+
